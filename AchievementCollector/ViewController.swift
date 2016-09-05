a8a3889da3d8e73f1837d3c019fecd2e1bb1c71d
johnmontewhat <us4john@gmail.com>
2016-09-05 00:29:45 -0700

    Initial Commit

---
+++ AchievementCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AchievementCollector
+//
+//  Created by John Montejano on 9/5/16.
+//  Copyright © 2016 John Montejano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
